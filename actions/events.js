1a31fe848148894521ea2ee270482649f09ca99a
Saqlain <riyanahmed1703@gmail.com>
2024-10-05 10:37:55 +0530

    successfully fetching the events

--- actions/events.js
+++ actions/events.js
@@ -12,7 +12,7 @@ export async function createEvent(data) {
       clerkUserId: userId,
     },
   });
-   console.log("BullShit.....",validatedSchema);
+  console.log("BullShit.....", validatedSchema);
   if (!user) throw new Error("User not found");
   const event = await db.event.create({
     data: {
@@ -22,4 +22,30 @@ export async function createEvent(data) {
   });
   console.log("event", event);
   return event;
+}
+
+export async function getUserEvents() {
+  const { userId } = auth();
+  if (!userId) {
+    throw new Error("Unauthorized");
+  }
+
+  const user = await db.user.findUnique({
+    where: { clerkUserId: userId },
+  });
+
+  if (!user) {
+    throw new Error("User not found");
+  }
+
+  const events = await db.event.findMany({
+    where: { userId: user.id },
+    orderBy: { createdAt: "desc" },
+    include: {
+      _count: {
+        select: { bookings: true },
+      },
+    },
+  });
+  return {events, username: user.username};
 }
--- app/(routes)/events/page.jsx
+++ app/(routes)/events/page.jsx
@@ -1,11 +1,30 @@
-"use client";
-export default function Events() {
+import { Suspense } from "react";
+import { getUserEvents } from "../../../actions/events";
+import EventCard from "../../../components/EventCard";
+export default function EventsPage(){
+  return(
+    <Suspense fallback={<div>Loading...</div>}>
+      <Events />
+    </Suspense>
+  )
+}
+
+async function Events () {
+   const {events, username} = await getUserEvents()
+   if(events.length === 0){
+      return <p>You have&apos;t created any events yet.</p>
+    }
+  //  console.log("events",events, "username", username)
   return (
-    <div className="w-full lg:w-[80%] lg:p-5">
-      <h1 className="text-6xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-blue-600 to-blue-400">
-        Events
+    <div className="w-full space-y-5 lg:w-[80%] lg:p-5">
+    <h1 className="text-4xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-blue-600 to-blue-400 text-center pt-5 lg:text-6xl lg:text-left">
+      Events
       </h1>
-      <span>Events content</span>
+      <div className="grid gap-8 grid-cols-1 lg:grid-cols-2">
+    {events.map((event) => (
+      <EventCard key={event.id} event={event} username={username} />
+    ))}
+      </div>
     </div>
   );
 }
